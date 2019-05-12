ac5b77bd42bedea9e3653a274445491156ab1556
Geliang Tian <tglasd@163.com>
2019-05-12 17:35:45 +0800

    project init

---
+++ src/main/java/cn/geliang/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package cn.geliang.mmall.dao;
+
+import cn.geliang.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/cn/geliang/mmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package cn.geliang.mmall.dao;
+
+import cn.geliang.mmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
