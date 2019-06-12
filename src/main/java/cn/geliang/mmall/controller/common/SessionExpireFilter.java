b2213b3114df2071c82120b5be566ba1841cf4c6
Geliang Tian <tglasd@163.com>
2019-06-12 20:01:35 +0800

    add session expire filter

---
+++ src/main/java/cn/geliang/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,41 @@
+package cn.geliang.mmall.controller.common;
+
+import cn.geliang.mmall.common.Const;
+import cn.geliang.mmall.pojo.User;
+import cn.geliang.mmall.util.CookieUtill;
+import cn.geliang.mmall.util.JsonUtil;
+import cn.geliang.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 对于每一次访问都重置token的有效时间
+ */
+public class SessionExpireFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String loginToken = CookieUtill.readLoginToken(httpServletRequest);
+        if (StringUtils.isNotEmpty(loginToken)) {
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obje(userJsonStr, User.class);
+            if (user != null) {
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        chain.doFilter(request, response;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
